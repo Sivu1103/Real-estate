da8fa4103f5c23d8bd1ebe7b11f5195a482bdf06
Sivavarshini <sivu1103@gmail.com>
2024-09-26 23:24:53 +0530

    complete upload listing images  functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-/*import {
+import {
   getDownloadURL,
   getStorage,
   ref,
@@ -7,33 +7,53 @@ import { useState } from 'react';
 } from 'firebase/storage';
 import { app } from '../firebase';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';*/
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateListing() {
-  /*const { currentUser } = useSelector((state) => state.user);
-  const navigate = useNavigate();
+  //const { currentUser } = useSelector((state) => state.user);
+  //const navigate = useNavigate();
   const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState({
     imageUrls: [],
-    name: '',
-    description: '',
-    address: '',
-    type: 'rent',
-    bedrooms: 1,
-    bathrooms: 1,
-    regularPrice: 50,
-    discountPrice: 0,
-    offer: false,
-    parking: false,
-    furnished: false,
+    //name: '',
+    //description: '',
+    //address: '',
+    //type: 'rent',
+    //bedrooms: 1,
+    //bathrooms: 1,
+    //regularPrice: 50,
+    //discountPrice: 0,
+    //offer: false,
+    //parking: false,
+    //furnished: false,
   });
+  
+  console.log(formData);
+
+  
+  
+  //const [formData, setFormData] = useState({
+    //imageUrls: [],
+    //name: '',
+    //description: '',
+    //address: '',
+    //type: 'rent',
+    //bedrooms: 1,
+    //bathrooms: 1,
+    //regularPrice: 50,
+    //discountPrice: 0,
+    //offer: false,
+    //parking: false,
+    //furnished: false,
+  //});
   const [imageUploadError, setImageUploadError] = useState(false);
   const [uploading, setUploading] = useState(false);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  console.log(formData);
+  //const [error, setError] = useState(false);
+  //const [loading, setLoading] = useState(false);
+  //console.log(formData);
+  
   const handleImageSubmit = (e) => {
-    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+    if (files.length > 0 && files.length + formData.imageUrls.length  < 7) {
       setUploading(true);
       setImageUploadError(false);
       const promises = [];
@@ -53,7 +73,7 @@ export default function CreateListing() {
         .catch((err) => {
           setImageUploadError('Image upload failed (2 mb max per image)');
           setUploading(false);
-        });
+      });
     } else {
       setImageUploadError('You can only upload 6 images per listing');
       setUploading(false);
@@ -91,7 +111,7 @@ export default function CreateListing() {
       imageUrls: formData.imageUrls.filter((_, i) => i !== index),
     });
   };
-
+/*
   const handleChange = (e) => {
     if (e.target.id === 'sale' || e.target.id === 'rent') {
       setFormData({
@@ -304,7 +324,7 @@ export default function CreateListing() {
           </p>
           <div className='flex gap-4'>
             <input
-             
+             onChange={(e) => setFiles(e.target.files)}
               className='p-3 border border-gray-300 rounded w-full'
               type='file'
               id='images'
@@ -313,23 +333,55 @@ export default function CreateListing() {
             />
             <button
               type='button'
-              
+              disabled={uploading}
+              onClick={handleImageSubmit}
               className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'
             >
-              Upload
+              {uploading ? 'Uploading...' : 'Upload'}
             </button>
           </div>
-          
+
+          <p className='text-red-700 text-sm'>
+            {imageUploadError && imageUploadError}
+          </p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, index) => (
+              <div
+                key={url}
+                className='flex justify-between p-3 border items-center'
+              >
+                <img
+                  src={url}
+                  alt='listing image'
+                  className='w-20 h-20 object-contain rounded-lg'
+                />
+                <button
+                  type='button'
+                  onClick={() => handleRemoveImage(index)}
+                  className='p-3 text-red-700 rounded-lg uppercase hover:opacity-75'
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+
+
           <button
           
             className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
           >
            Create Listing
           </button>
-        
+         
         </div>
+        
       </form>
     </main>
   );
 
 }
+
+
+
+ git config --global user.email "sivu1103.com"
+  git config --global user.name "Sivu1103"
